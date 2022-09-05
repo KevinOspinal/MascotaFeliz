cc4e0e5671af2892a50274576f731d30a293672c
SulyJaime <102184335+SulyJaime@users.noreply.github.com>
2022-09-05 07:36:30 -0500

    Rama Suly

--- MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+using System.Collections.Generic;
 
 namespace MascotaFeliz.App.Consola
 {
     class Program
     {
+        private static IRepositorioDueno _repoDueno = new RepositorioDueno (new Persistencia.AppContext());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola amigos vamos a trabajar en las tablas");
+        }
+        //agregar dueño
+        private static void AddDueno()
+        {
+            var dueno = new Dueno
+            {
+                Nombres = "Leonardo",
+                Apellidos = "Rodriguez", 
+                Direccion = "Calle 120",
+                Telefono = "111258258",
+                Correo = "Batman@gmail.com"
+            };
+            _repoDueno.AddDueno(dueno);
         }
     }
 }
