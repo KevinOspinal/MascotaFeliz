1d0845d9f58c1f3030679f39fc39debe7d83509d
KevinOspinal <102458199+KevinOspinal@users.noreply.github.com>
2022-09-07 10:59:02 -0500

    Copia K

--- MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+using System.Collections.Generic;
 namespace MascotaFeliz.App.Consola
 {
     class Program
     {
+        private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddDueno();
+        }
+
+        private static void AddDueno()
+        {
+            var dueno = new Dueno
+            {
+                //Cedula = "1212",
+                Nombre = "Juan",
+                Apellido = "Sin Miedo",
+                Direccion = "Bajo un puente",
+                Telefono = 1234567891,
+                Email = "juansinmiedo@gmail.com"
+            };
+            _repoDueno.AddDueno(dueno);
         }
     }
+
+    
 }
